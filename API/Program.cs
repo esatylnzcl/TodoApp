325cc1c5c9c2905d777f962de6ec2ff34f4e05bc
esatylnzcl <esatylnzcl@gmail.com>
2026-01-17 16:25:03 +0300

    auto migration added

--- API/Program.cs
+++ API/Program.cs
@@ -105,4 +105,22 @@ app.UseAuthorization();
 
 app.MapControllers(); 
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        if (context.Database.GetPendingMigrations().Any())
+        {
+            context.Database.Migrate();
+        }
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Veritabanı otomatik migrate edilirken hata oluştu.");
+    }
+}
+
 app.Run();
